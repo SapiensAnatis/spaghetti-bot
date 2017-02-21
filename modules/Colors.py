cc14b45c74ee0f7640daf09475935fcf5d9a90bc
SapiensAnatis <jay0@me.com>
2017-02-21 23:02:23 +0000

    Various cleanup jobs, change role names
    I can't believe I didn't kill myself with excessive use of format like
    that
    
    I don't care that this new way requires python 3.6+, it's so much better
    and you're basically a barbarian if you're still on 3.5 at this point,
    solely because of this.
    
    Also: role names are now [<colorcode>] rather than (<colorcode>) as
    before.

--- modules/Colors.py
+++ modules/Colors.py
@@ -10,13 +10,13 @@ cleanup_delay = 6000  # seconds
 
 
 def role_has_users(role):
-    if role.name.startswith("(#"):
+    if role.name.startswith("[#"):
         people_with_role = [x.name for x in role.server.members if role in x.roles]
-    #   print("[debug][Colors.py] Role search complete for role {0}: found users {1}".format(role.name, people_with_role))
+    #   print(f"[debug][Colors.py] Role search complete for role {role.name}: found users {people_with_role}")
         return bool(people_with_role)
     else:
-        print("[warn][Colors.py] Attempted to check role_has_users status of a role {0}, which is not owned by this script!"
-                .format(role.name))
+        print(f"[warn][Colors.py] Attempted to check role_has_users status of a role {role.name}! "
+              f"This role is not owned by this script!")
         return True
 
 
@@ -29,26 +29,23 @@ class Colors():
     async def start_background_clean(self):
         await self.bot.wait_until_ready()
         while True:
-            print("[Colors.py] Cleaning up unused & duplicate colour roles..."
-                  .format(cleanup_delay))
+            print("[Colors.py] Cleaning up unused & duplicate colour roles...")
             count = 0
             for server in self.bot.servers:
                 roles = []
                 for role in server.roles:
-                    if role.name.startswith("(#"):  # make sure it's our business
+                    if role.name.startswith("[#"):  # make sure it's our business
                         if not role_has_users(role):
-                            print("\t* L O C A T E D unused role {0} in server {1}. Deleting..."
-                                  .format(role, server))
+                            print(f"\t* L O C A T E D redundant role {role} in server {server}. Deleting...")
                             await self.bot.delete_role(server, role)
                             count += 1
                         if role.name in roles:
-                            print("\t* L O C A T E D duplicate role {0} in server {1}. Deleting..."
-                                  .format(role, server))
+                            print(f"\t* L O C A T E D duplicate role {role} in server {server}. Deleting...")
                             await self.bot.delete_role(server, role)
                             count += 1
 
                         roles.append(role.name)
-            print("[Colors.py] Finished cleaning up colour roles; {0} roles deleted.".format(count))
+            print(f"[Colors.py] Finished cleaning up colour roles; {count} roles deleted.")
             await sleep(6000)
 
     @commands.command(pass_context=True, aliases=['colour'])
@@ -62,53 +59,49 @@ class Colors():
 
         color_argument = color_argument_raw.replace("#", "")  # remove hash; the int(hex, 16) method dislikes hashes
 
-        print("[Colors.py on server {2}] User {0} has requested a colour change to {1}."
-              .format(author.name, color_argument, server.name))
+        print("[Colors.py on server {server.name}] User {author.name} has requested a colour change to #{color_argument}.")
         # Ensure validity of hex code
         if len(color_argument) != 6:  # Potentially invalid (alpha channels are obviously not supported)
             await self.bot.send_message("Please provide a valid hex code.", ctx.message.channel)
             return
-        print("\t* Input was validated successfully. Cleaning up old roles...")
+        print("\t* The user's input was validated successfully. Removing existing roles...")
 
         # Find a user's existing colour role if applicable
-        old_role = discord.utils.find(lambda role: role.name.startswith("(#"), author.roles)
+        old_role = discord.utils.find(lambda role: role.name.startswith("[#"), author.roles)
 
         if old_role is not None:  # if one was found
-            print("\t\t* Found existing colour role {0} - removing..."
-                  .format(old_role.name))
+            print(f"\t\t* Found existing colour role {old_role.name} - removing...")
             await self.bot.remove_roles(author, old_role)
-            await sleep(0.2)  # dirty hack to make sure delete actually goes through before we check for unuse
+            await sleep(0.2)  # dirty hack to make sure delete actually goes through before we check for disuse
             print("\t\t* Removed role, checking if still in use...")
             # If that role is now empty, remove it entirely:
             if not role_has_users(old_role):  # if there is nobody with the role
                 print("\t\t* Existing colour role was now empty, deleting...")
                 await self.bot.delete_role(server, old_role)
 
-        print("\t* Old roles successfully cleaned. Applying and possibly creating new role...")
+        print("\t* Existing roles successfully cleaned. Applying and possibly creating new role...")
         # Now, apply the new role. First, check if the role already exists
-        result = discord.utils.find(lambda role: role.name == "(#{0})".format(color_argument), server.roles)
+        result = discord.utils.find(lambda role: role.name == f"[#{color_argument}])", server.roles)
         if result is not None:
-            print("\t\t* Provided colour change to {0} for {1} using pre-existing role."
-                  .format(color_argument, author.name))
+            print(f"\t\t* Provided colour change to {color_argument} for {author.name} using pre-existing role.")
             await self.bot.add_roles(author, result)
         else:
-            print(
-                "\t\t* Cannot provide colour change to hex {0} for {1} using existing roles. Creating new role for them..."
-                .format(color_argument, author.name))
+            print(f"\t\t* Cannot provide colour change to hex {color_argument} for {author.name} using existing roles. "
+                  f"Creating new role for them...")
 
             color_int = int(color_argument,
                             16)  # convert our string to a hex int with which we can generate a discord.Color
             discord_color = discord.Color(color_int)
-            new_role = await self.bot.create_role(server, name="(#{0})".format(color_argument),
+            new_role = await self.bot.create_role(server, name=f"[#{color_argument}]",
                                                   color=discord_color, permissions=discord.Permissions.none())
+
             ''''"To the top!" - /r/the_donald, literally every day
-            # print("Created. Moving to pos {0}..."
-            #      .format(server.me.top_role.position - 1), end="")
+            # print(f"Created. Moving to pos {server.me.top_role.position - 1}...", end="")
             # await self.Session.move_role(server, new_role, server.me.top_role.position - 1)
 
-            print("\t\t* Successfully created role ({0}) for user {1}. Applying..."
-                  .format(color_argument, author.name))'''
-            # The above code causes permission errors for some reason. It is ultimately unimportant, as it seems unlikely
+            print(f"\t\t* Successfully created role ({color_argument}) for user {author.name}. Applying...")'''
+
+            # The above code causes permission errors for some reason. It is ultimately unimportant;  it seems unlikely
             # to me that admins will make roles that have colours overriding ours without a specific reason in mind.
             # Moreover, a user can only have one at a time. Due to its unimportance, a fix isn't worth pursuing.
 
